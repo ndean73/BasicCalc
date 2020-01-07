a700d5ca2b6fcbf93acfefd59ae74471765eb27c
ndean73 <Dean.Nalumpa@mlnr.gov.zm>
2020-01-07 13:17:17 -0800

    Task: Created Librarys for Addition, Subtraction,Division and Multiplication

---
+++ MathOperations/Square.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathOperations
+{
+    ////Square
+    public class Square
+    {
+
+
+        public static double Sqr(double a,double b)
+        {
+
+            return Math.Pow(a,b);
+
+        }
+
+        public static int Sqr(int b)
+        {
+            int c = 0;
+
+            c = Sqr(b);
+
+
+            return c;
+        }
+    }
+}
